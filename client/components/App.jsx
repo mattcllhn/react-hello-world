3f59a9a26b4142f65122d1f897cc45efe363c893
Matt Callahan <mattcllhn@gmail.com>
2017-02-03 09:52:08 -0600

    first

---
+++ client/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <div style = {{textAlign:'center'}}>
+          <h1> HOLY BALLS THIS A REACT COMPONENT</h1>
+      </div>
+    );
+  }//render
+}//export class
+// import React from 'react';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//      <div style={{textAlign: 'center'}}>
+//         <h1>Hello World</h1>
+//       </div>);
+//   }
+// }
---
+++ client/index.js
@@ -0,0 +1,6 @@
+console.log('js works');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
